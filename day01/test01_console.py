ce50f5fc282b6720eb6485ccf4d92708badcf903
YooChangWoo <dbckddn1201@naver.com>
2024-01-29 16:35:13 +0900

    최종 처리

---
+++ day01/test01_console.py
@@ -0,0 +1,8 @@
+#  date : 2024-01-29
+#  desc : 콘솔출력
+
+# 콘솔출력을 위해서는 print() 함수를 사용 (주석 : 소스 실행에 영향을 주지 않음)
+print('Hello, World!')
+
+#  > python test01_console.py 로 콘솔실행
+#  VS Code에서 Ctrl + f5(실행 > 디버깅 없이 실행)
